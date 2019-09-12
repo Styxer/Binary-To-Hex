fe2d006031561659afa8977212fa53bd3926b1a6
ofir <gennin@gmail.com>
2019-09-12 18:07:01 +0300

    First Draft

---
+++ Binary-To-Hex/Binary-To-Hex/ItemEntry.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_To_Hex
+{
+    public  class ItemEntry
+    {
+        public ItemEntry(int name, bool isSelected)
+        {
+            Name = name;
+            IsSelected = isSelected;
+        }
+
+        public int Name { get; set; }
+        public bool IsSelected { get; set; }
+        DelegateCommand<string> CheckedChnaged;
+    }
+}
---
+++ Binary-To-Hex/Binary-To-Hex/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace Binary_To_Hex.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _hexNumber;
+        public string HexNumber
+        {
+            get { return _hexNumber; }
+            set { SetProperty(ref _hexNumber, value); }
+        }
+
+        private ObservableCollection<ItemEntry> _extensionGroups;
+        public  ObservableCollection<ItemEntry> ExtensionGroups
+        {
+            get { return _extensionGroups; }
+            set { SetProperty(ref _extensionGroups, value); }
+        }
+
+
+        #region Comamnds
+
+        public DelegateCommand CheckedChnaged { get;  set; }
+
+
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            _extensionGroups = new ObservableCollection<ItemEntry>();
+
+            for (int i = 0; i < 32; i++)
+            {
+                ExtensionGroups.Add(new ItemEntry(i, true));
+            }
+
+            CheckedChnaged = new DelegateCommand(CheckedChnagedExecute);
+
+            HexNumber = "‭FFFFFFFF‬";
+
+        }
+
+     
+
+      
+
+        #region Function
+        private void CheckedChnagedExecute()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in ExtensionGroups)
+            {
+                if (item.IsSelected)
+                    builder.Append("1");
+                else
+                    builder.Append("0");
+            }
+            HexNumber = HexConverted(builder.ToString());
+        }
+
+        string HexConverted(string strBinary)
+        {
+            string strHex = Convert.ToInt32(strBinary, 2).ToString("X");
+            return strHex;
+        }
+        #endregion
+    }
+}
